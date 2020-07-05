e1a6d027822254296a8b7103c6f2cbd244cafe7f
CottonTheButton <elhison08@gmail.com>
2020-07-05 09:32:37 +0800

    Fixed the role assignment feature thing

--- cogs/self.py
+++ cogs/self.py
@@ -13,7 +13,7 @@ class Self(commands.Cog):
         guild = discord.utils.get(self.bot.guilds, name="Cotton Education Center")
         role_id = discord.utils.get(guild.roles, name=role_name)
         await ctx.channel.purge(limit=1)
-        if role_name == "KGB spy" or role_name == "Cardinal" or role_name == "Small pp" or role_name == "Big pp" or role_name == "Pope (entshuldigung)" or role_name == "Pyrocynical's Brother" or role_name == "God" or role_name == "Jesus Christ":
+        if role_name == "KGB spy" or role_name == "Cardinal" or role_name == "Small pp" or role_name == "Big pp" or role_name == "Pope (entshuldigung)" or role_name == "Pyrocynical's Brother" or role_name == "God" or role_name == "Jesus Christ" or role_name == "Science Teacher" or role_name == "Programming Teacher" or role_name == "Math Teacher" or role_name == "Counsellor" or role_name == "Business Teacher" or role_name == "Linguistics Teacher" or role_name == "Music Teacher" or role_name == "Carbon-based life form" or role_name == "Unverified" or role_name == "Peasant Bot" or role_name == "@everyone" or role_name == "@@everyone":
             ctx.send("Not allowed!")
         else:
             await ctx.send(f"Gave {ctx.author} a {role_id} role!")
