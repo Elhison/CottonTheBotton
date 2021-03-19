72a624a9d37484d665b61b531b9d440d8912e5ae
CottonTheButton <elhison08@gmail.com>
2021-03-19 16:16:19 +0800

    Added status presence I copied and pasted the example code thing from Discord.py's documentation

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import antigravity
+import discord
+
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="with my pp"))
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+pwd_file = open("bruh.txt")
+
+pwd = pwd_file.readlines()
+print(pwd)
+client.run(pwd[0])
